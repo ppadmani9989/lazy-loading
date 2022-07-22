854801646f23f26fa1cf81a4c7c27114a2dadb17
Pooja Padmani <35580282+ppadmani9989@users.noreply.github.com>
2022-07-22 13:42:34 -0500

    Adding changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) 
+  }, 
+  { 
+    path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) 
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
